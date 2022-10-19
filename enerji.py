0e9f389a65710ddbcb44521a897fdccbc2aca985
Ahmet Kasap <aahmetkasap@gmail.com>
2022-10-19 15:44:20 +0300

    move

--- enerji.py
+++ enerji.py
@@ -1,32 +1,36 @@
 import pyautogui
 import time
 
-def get_item() :
-    pyautogui.displayMousePosition(300,300) # oyun içerisinde silah satıcısını bul  #güncellenecek
-    time.sleep(0.2)
+'''
+bot dışında karekteri silah satıcısına bırak => item al => simyacıyıya git => itemleri simyacı üzerine bırak => 
+işlem bittikten sonra tekrar silah satıcısına git => işlemler tekrarlansın.
+''' 
+
+
+def start(x,y): 
+    pyautogui.leftClick()(x,y)
     
-    pyautogui.doubleClick(300,300) # marketi aç
+
+def get_item() :
+    pyautogui.displayMousePosition(300,300) # silah satıcısı kordinantı
+    time.sleep(0.2)
+    pyautogui.leftClick()(300,300)
+    time.sleep(0.1)
+    pyautogui.press('enter') # silah satıcısını aç
     time.slep(0.2)
     for i in range (1,80) : 
         pyautogui.leftClick(450,700) # marketten 80 tane item al # envanterin ilk 10 satırına 10 adet enerji parçası önceden koy
         time.sleep(0.7)
     
     
-'''  
-def throw_item() : # bir item 
-    pyautogui.displayMousePosition(750,578) # simyacıyı bul 
-    time.sleep(0.2)
-    
-    pyautogui.rightClick(750,15) # tek bir itemi seçme
-    time.sleep(0.1)
-    pyautogui.moveTo(400,133)  # itemi simyacı üzerine bırakma
-    time.sleep(0.1)
-    pyautogui.press("enter") # çıkan yazıyı onaylama
-    
-'''  
+def alchemistry_go(): # silah satıcısından simyacıya gidiş   #güncellenecek
+    pass
 
-def throw_item () : # bir çok itemi sırayla simyacı üzerine bırakma 
-    pyautogui.displayMousePosition(750,578) # simyacıyı bul   #güncellenecek
+def gun_market() : # simyacıdan silah satıcısına gidiş.   #güncellenecek
+    pass
+
+def throw_item () : #itemleri tek tek simyacı üzerine bırakıp enerji parçası üretme
+    pyautogui.displayMousePosition(750,578) # simyacıyı kordinantı
     time.sleep(0.2)
     
     # +65 her seferinde
@@ -47,57 +51,65 @@ def throw_item () : # bir çok itemi sırayla simyacı üzerine bırakma
     y8 = 550
     y9 = 600
     
+    
     for i in range (1,2) : 
     
         for i in range (1,9) : 
-            pyautogui.rightClick(x1,y1) # itemi seçme
-            time.sleep(0.1)
-            pyautogui.moveTo(400,133)  # itemi simyacı üzerine bırakma
-            time.sleep(0.1)
-            pyautogui.press("enter") # çıkan yazıyı onaylama
-            y1 = y1 + 50 
-            
-        for i in range (1,9) : 
-            pyautogui.rightClick(x2,y1) # itemi seçme
-            time.sleep(0.1)
-            pyautogui.moveTo(400,133)  # itemi simyacı üzerine bırakma
-            time.sleep(0.1)
-            pyautogui.press("enter") # çıkan yazıyı onaylama
-            y1 = y1 + 50
-            
-        for i in range (1,9) : 
-            pyautogui.rightClick(x3,y1) # itemi seçme
-            time.sleep(0.1)
-            pyautogui.moveTo(400,133)  # itemi simyacı üzerine bırakma
+            pyautogui.leftClick(x1,y1) # itemi seçme
+            pyautogui.mouseDown() 
+            pyautogui.moveTo(400,133) # itemi simyacı üzerine bırakma
+            pyautogui.mouseUp()
             time.sleep(0.1)
             pyautogui.press("enter") # çıkan yazıyı onaylama
             y1 = y1 + 50 
             
         for i in range (1,9) : 
-            pyautogui.rightClick(x4,y1) # itemi seçme
-            time.sleep(0.1)
-            pyautogui.moveTo(400,133)  # itemi simyacı üzerine bırakma
+            pyautogui.leftClick(x1,y1) # itemi seçme
+            pyautogui.mouseDown() 
+            pyautogui.moveTo(400,133) # itemi simyacı üzerine bırakma
+            pyautogui.mouseUp()
             time.sleep(0.1)
             pyautogui.press("enter") # çıkan yazıyı onaylama
             y1 = y1 + 50 
             
         for i in range (1,9) : 
-            pyautogui.rightClick(x5,y1) # itemi seçme
+            pyautogui.leftClick(x1,y1) # itemi seçme
+            pyautogui.mouseDown() 
+            pyautogui.moveTo(400,133) # itemi simyacı üzerine bırakma
+            pyautogui.mouseUp()
             time.sleep(0.1)
-            pyautogui.moveTo(400,133)  # itemi simyacı üzerine bırakma
+            pyautogui.press("enter") # çıkan yazıyı onaylama
+            y1 = y1 + 50 
+            
+        for i in range (1,9) : 
+            pyautogui.leftClick(x1,y1) # itemi seçme
+            pyautogui.mouseDown() 
+            pyautogui.moveTo(400,133) # itemi simyacı üzerine bırakma
+            pyautogui.mouseUp()
+            time.sleep(0.1)
+            pyautogui.press("enter") # çıkan yazıyı onaylama
+            y1 = y1 + 50 
+            
+        for i in range (1,9) : 
+            pyautogui.leftClick(x1,y1) # itemi seçme
+            pyautogui.mouseDown() 
+            pyautogui.moveTo(400,133) # itemi simyacı üzerine bırakma
+            pyautogui.mouseUp()
             time.sleep(0.1)
             pyautogui.press("enter") # çıkan yazıyı onaylama
             y1 = y1 + 50 
         
-        pyautogui.rightClick(750,15) # 2. envanteri aç ve işlem döngüsü 2. defa çalışsın.
+        pyautogui.leftClick(750,15) # 2. envanteri aç ve işlem döngüsü 2. defa çalışsın.
         
         
+start(500,500) # girilen cordinat ile oyuna tıkla / başla
+time.sleep(1)
+for i in range (1,15):     
+    get_item()
+    alchemistry_go()
+    throw_item()
+    gun_market()
     
-  
-    
-    
-get_item()
-throw_item()
 
-def end () : # 10 veya 20 paket enerji parçası yapıldıktan sonra bunu depoya aktarmak lazım. İlk aşamada el ile yapılabilir.  #güncellenecek
+def end () : # 10 veya 20 paket enerji parçası yapıldıktan sonra bunu depoya aktarmak lazım. İlk aşamada el ile yapılabilir.
     time.sleep(5400)

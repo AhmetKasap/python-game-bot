96de31827cab0777236f0c6e69b7022c15fcfdcb
Ahmet Kasap <aahmetkasap@gmail.com>
2022-11-07 00:41:40 +0300

    end

--- enerji.py
+++ enerji.py
@@ -1,14 +1,11 @@
 import pyautogui
 import time
+from python_imagesearch import imagesearch
 
 def start(x,y): 
     pyautogui.leftClick(x,y)
 
-
-'''
-from python_imagesearch import imagesearch
-
-def get_item2():
+def get_item():
     while (True) :
         market = imagesearch.imagesearch("market.png")
         if market[0] != -1:
@@ -21,18 +18,6 @@ def get_item2():
             break
         else:
             print("market açılamadı, tekrar açmayı dene")
-'''
-
-def get_item() :
-    pyautogui.moveTo(680,363) #silah satıcısı kordinantı
-    time.sleep(0.1)
-    pyautogui.click(button='left', clicks=1) #silah satıcısına tıkla
-    time.sleep(0.1)
-    pyautogui.moveTo(680, 300) #silah satıcısını aç
-    pyautogui.click(button='left', clicks=1) #marketi aça tıkla
-    for i in range (1,80) : # item al
-        pyautogui.leftClick(450,700) 
-        time.sleep(0.7)
 
 
 def go_alchemistry () :  #ekran klavyesi ile çalışır, simyacıya gidiş
@@ -68,33 +53,7 @@ def go_alchemistry () :  #ekran klavyesi ile çalışır, simyacıya gidiş
     pyautogui.mouseDown()
     time.sleep(2)
     pyautogui.mouseUp()
-    
-'''    
-def go_alchemistry () :
-    for i in range (1,45): #4.5 sn
-        pyautogui.press('s')
-    
-    for i in range (1,10):  # 1 sn
-        pyautogui.press('e')
-        
-    for i in range (1,120): # 12sn 
-        pyautogui.press('w')
-    
-    for i in range (1,20):  # 2 sn 
-        pyautogui.press('q')
-    
-    for i in range (1,20):  # 2 sn 
-        pyautogui.press('w')
-'''
-'''
-def go_alchemistry () :
-    #15 press 1 sn
-    pyautogui.press('s', presses=68, interval=0.02)  # 4.5 sn
-    pyautogui.press('e', presses=15, interval=0.02)  # 1 sn
-    pyautogui.press('w', presses=180, interval=0.02) # 12 sn
-    pyautogui.press('q', presses=30, interval=0.02)  # 2 sn
-    pyautogui.press('w', presses=30, interval=0.02)  # 2 sn
-'''
+
 
 
 def go_market () :  #ekran klavyesi ile çalışır, silah  satıcısına gidiş
@@ -130,41 +89,9 @@ def go_market () :  #ekran klavyesi ile çalışır, silah  satıcısına gidiş
     pyautogui.mouseDown()
     time.sleep(6)
     pyautogui.mouseUp()
-'''
-def go_market () :
-    for i in range (1,30): #3 sn
-        pyautogui.press('s')
-    
-    for i in range (1,15):  # 1.5 sn
-        pyautogui.press('q')
-        
-    for i in range (1,70): # 7sn 
-        pyautogui.press('w')
-    
-    for i in range (1,3):  # 0.3 sn 
-        pyautogui.press('e')
-    
-    for i in range (1,65):  # 6.5 sn 
-        pyautogui.press('w')
-        
-    for i in range (1,2):  # 0.2 sn 
-        pyautogui.press('e')
-        
-    for i in range (1,15):  # 1.5 sn 
-        pyautogui.press('w')
-'''
-'''
-def go_market () :
-    #15 press 1 sn
-    pyautogui.press('s', presses=45, interval=0.02)  # 3 sn
-    pyautogui.press('q', presses=21, interval=0.02)  # 1.5 sn
-    pyautogui.press('w', presses=105, interval=0.02) # 7 sn
-    pyautogui.press('e', presses=5, interval=0.02)  # 0.3 sn
-    pyautogui.press('w', presses=98, interval=0.02)  # 6.5 sn
-    pyautogui.press('e', presses=3, interval=0.02)  # 0.2 sn
-    pyautogui.press('w', presses=21, interval=0.02)  # 1.5 sn
 
-'''
+
+
 def throw_item () : 
     pyautogui.displayMousePosition(750,578) # simyacıyı kordinantı
     time.sleep(0.2)
@@ -188,8 +115,7 @@ def throw_item () :
     #y7 = 500
     #y8 = 550
     #y9 = 600
-    
-    
+
     for i in range (1,2) : 
     
         for i in range (1,9) : 
@@ -249,7 +175,7 @@ def throw_item () :
         
         pyautogui.leftClick(750,15) # 2. envanteri aç ve işlem döngüsü 2. defa çalışsın.
         
-        
+
 start(500,500) 
 time.sleep(1)
 for i in range (1,15):     
@@ -257,5 +183,94 @@ for i in range (1,15):
     go_alchemistry()
     throw_item()
     go_market()
+
+
+
+
+
+
+'''
+OLD
+
+def get_item() :
+    pyautogui.moveTo(680,363) #silah satıcısı kordinantı
+    time.sleep(0.1)
+    pyautogui.click(button='left', clicks=1) #silah satıcısına tıkla
+    time.sleep(0.1)
+    pyautogui.moveTo(680, 300) #silah satıcısını aç
+    pyautogui.click(button='left', clicks=1) #marketi aça tıkla
+    for i in range (1,80) : # item al
+        pyautogui.leftClick(450,700) 
+        time.sleep(0.7)   
+
+def go_alchemistry () :
+    for i in range (1,45): #4.5 sn
+        pyautogui.press('s')
+    
+    for i in range (1,10):  # 1 sn
+        pyautogui.press('e')
+        
+    for i in range (1,120): # 12sn 
+        pyautogui.press('w')
+    
+    for i in range (1,20):  # 2 sn 
+        pyautogui.press('q')
+    
+    for i in range (1,20):  # 2 sn 
+        pyautogui.press('w')
+
+def go_alchemistry () :
+    #15 press 1 sn
+    pyautogui.press('s', presses=68, interval=0.02)  # 4.5 sn
+    pyautogui.press('e', presses=15, interval=0.02)  # 1 sn
+    pyautogui.press('w', presses=180, interval=0.02) # 12 sn
+    pyautogui.press('q', presses=30, interval=0.02)  # 2 sn
+    pyautogui.press('w', presses=30, interval=0.02)  # 2 sn
+
+def go_market () :
+    for i in range (1,30): #3 sn
+        pyautogui.press('s')
+    
+    for i in range (1,15):  # 1.5 sn
+        pyautogui.press('q')
+        
+    for i in range (1,70): # 7sn 
+        pyautogui.press('w')
+    
+    for i in range (1,3):  # 0.3 sn 
+        pyautogui.press('e')
+    
+    for i in range (1,65):  # 6.5 sn 
+        pyautogui.press('w')
+        
+    for i in range (1,2):  # 0.2 sn 
+        pyautogui.press('e')
+        
+    for i in range (1,15):  # 1.5 sn 
+        pyautogui.press('w')
+
+def go_market () :
+    #15 press 1 sn
+    pyautogui.press('s', presses=45, interval=0.02)  # 3 sn
+    pyautogui.press('q', presses=21, interval=0.02)  # 1.5 sn
+    pyautogui.press('w', presses=105, interval=0.02) # 7 sn
+    pyautogui.press('e', presses=5, interval=0.02)  # 0.3 sn
+    pyautogui.press('w', presses=98, interval=0.02)  # 6.5 sn
+    pyautogui.press('e', presses=3, interval=0.02)  # 0.2 sn
+    pyautogui.press('w', presses=21, interval=0.02)  # 1.5 sn
+
+'''
+
+
+
+
+
+
+
+
+
+
+        
+
     
 
